b6394fcf95edd15af8b79117237ac27f56af1b76
lisp719 <368034+lisp719@users.noreply.github.com>
2024-08-26 00:59:21 +0900

    feat: Open google maps based on gps

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGeolocated } from "react-geolocated";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { coords } = useGeolocated({
+    positionOptions: { enableHighAccuracy: true },
+    watchPosition: true,
+    watchLocationPermissionChange: true,
+  });
+
+  const openMap = () => {
+    window.open(
+      `https://www.google.com/maps/search/${coords?.latitude},${coords?.longitude}`
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>latitude: {coords?.latitude}</p>
+      <p>longitude: {coords?.longitude}</p>
+      <button type="button" onClick={openMap}>
+        map
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
